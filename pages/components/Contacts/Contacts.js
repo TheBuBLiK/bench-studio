e6c2dd245c8644ecf0b18c986b89abc3eace923a
unknown <ataydjirgalbaev@gmail.com>
2021-10-12 18:35:39 +0600

    start doing sidebar

--- pages/components/Contacts/Contacts.js
+++ pages/components/Contacts/Contacts.js
@@ -2,8 +2,16 @@ const Contacts = () => {
     return (
         <div className="contacts">
             <div className="contacts__wpapper">
-                <div className="contacts__up"></div>
-                <div className="contacts__down"></div>
+                <div className="contacts__up">
+                    <p className="contacts__deserve">Вы этого заслуживаете!</p>
+                    <div className="contacts__contact">
+                        <a href="#" className="contacts__phone gold">
+                            +7 (8412) 68 68 98
+                        </a>
+                        <p className="contacts__shedule">asd</p>
+                    </div>
+                </div>
+                <div className="contacts__down">asd</div>
             </div>
         </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,10 @@ import Head from "next/head";
 import Image from "next/image";
 import Content from "./components/Content/Content";
 import Sidebar from "./components/Sidebar/Sidebar";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
     return (
-        <div>
+        <div className="main-container">
             <Sidebar />
             <Content />
         </div>
