8ae0a7fb60e89bea8f8e7e93d87edf76d85114e4
unknown <ataydjirgalbaev@gmail.com>
2021-10-13 21:57:39 +0600

    add Blocks path, make better sidebar

--- pages/components/Sidebar/Sidebar.js
+++ pages/components/Sidebar/Sidebar.js
@@ -1,28 +1,40 @@
+import asd from "../../../public/LOGO.svg";
 const Sidebar = () => {
     return (
         <nav className="sidebar">
             <div className="sidebar__wrapper">
                 <div className="sidebar__logo">
-                    <img src="/" />
+                    <img src={asd.src} alt="logo" />
                 </div>
                 <div className="sidebar__list">
                     <ul>
                         <li>
-                            <a href="#">Главная</a>
+                            <a href="#">Home</a>
                         </li>
                         <li>
-                            <a href="#">Главная</a>
+                            <a href="#">About</a>
                         </li>
                         <li>
-                            <a href="#">Главная</a>
+                            <a href="#">Values</a>
                         </li>
                         <li>
-                            <a href="#">Главная</a>
+                            <a href="#">Project start</a>
                         </li>
                         <li>
-                            <a href="#contacts">Главная</a>
+                            <a href="#contacts">Services</a>
+                        </li>
+                        <li>
+                            <a href="#contacts">Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="#contacts">Contact</a>
                         </li>
                     </ul>
+                </div>
+                <div className="sidebar__icons">
+                    <img src="/" />
+                    <img src="/" />
+                    <img src="/" />
                 </div>
             </div>
         </nav>
