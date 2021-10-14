7e380c1477dfcd1cd1b00ce1e0263342d26001bc
unknown <kozhon.amantay@gmail.com>
2021-10-14 17:36:35 +0600

    change old

--- pages/components/About/About.js
+++ pages/components/About/About.js
@@ -1,5 +1,12 @@
 const About = () => {
-  return <div></div>;
+  return (
+    <div className="about__container">
+      <div className="about__wrapper">
+        <div className="about__left"></div>
+        <div className="about__right"></div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
