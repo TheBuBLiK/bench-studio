f48dae80e39825f1633a950ec7410c5f17b3aa7a
unknown <kozhon.amantay@gmail.com>
2021-10-14 16:56:59 +0600

    test

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,12 @@ import Content from "./components/Content/Content";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 export default function Home() {
-    return (
-         <div className="main-container">
+  return (
+    <div className="main-container">
       <div className="main-wrapper">
         <Sidebar />
         <Content />
       </div>
     </div>
-    );
+  );
 }
