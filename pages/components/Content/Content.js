23de99d3a5e0ffae155a46b81597c9420af255bb
unknown <kozhon.amantay@gmail.com>
2021-10-15 19:22:45 +0600

    val blocks remake

--- pages/components/Content/Content.js
+++ pages/components/Content/Content.js
@@ -1,6 +1,7 @@
 import About from "../About/About";
 import Values from "../Values/Values";
 import ProjectStarts from "../ProjectStarts/ProjectStarts";
+import Contacts from "../Contacts/Contacts";
 
 const Content = () => {
   return (
